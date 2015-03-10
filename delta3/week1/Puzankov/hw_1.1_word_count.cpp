89b65560240c5ab005827426782219b8b1ee420d
Ivan Puzankov <12zrus@gmail.com>
2015-03-10 08:17:39 +0300

    Puzankov homework week1 c++ Added homework for c++11 tasks

---
+++ delta3/week1/Puzankov/hw_1.1_word_count.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
+
+std::mutex queue_mutex;
+std::mutex result_mutex;
+std::condition_variable cvar;
+std::atomic<bool> done;
+
+void count_words(std::queue<std::string> &lines, unsigned &result)
+{
+	std::string new_string = "";
+	std::stringstream ss;
+	while (!done || !lines.empty()) {
+		std::unique_lock<std::mutex> locked(queue_mutex);
+		while (lines.empty()) {
+			cvar.wait(locked);
+			if (done && lines.empty()) return;
+		}
+		ss.clear();
+		ss.str(lines.front());
+		lines.pop();
+		while (ss >> new_string) {
+			std::lock_guard<std::mutex> locked(result_mutex);
+			result++;
+		}
+	}
+}
+
+void read_line(std::ifstream &input, std::queue<std::string> &lines)
+{
+	std::string line = "";
+	while (!input.eof()) {
+		std::lock_guard<std::mutex> locked(queue_mutex);
+		std::getline(input, line);
+		lines.push(line);
+		cvar.notify_one();
+	}
+	done = true;
+	cvar.notify_all();
+}
+
+int main()
+{
+	std::ifstream input("input.txt");
+	unsigned result = 0;
+	std::queue<std::string> lines;
+	std::vector<std::thread> threads;
+	threads.push_back(std::thread(read_line, std::ref(input), std::ref(lines)));
+	unsigned nthreads = std::thread::hardware_concurrency() - 1;
+	for (unsigned i = 0; i < nthreads; ++i) {
+		threads.push_back(std::thread(count_words, std::ref(lines), std::ref(result)));
+	}
+	for (auto& t : threads) {
+		t.join();
+	}
+	input.close();
+	std::cout << "number of words: " << result << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ delta3/week1/Puzankov/hw_1.2_scalar_product.cpp
@@ -0,0 +1,49 @@
+﻿#include <thread>
+#include <mutex>
+#include <iostream>
+#include <vector>
+#include <time.h>
+
+#define N 100000
+std::mutex result_lock;
+
+void calc(std::vector<int> a, std::vector<int> b, int first, int last, int &res)
+{
+    int tmp = 0;
+    for (int i = first; i < last; i++) {
+        tmp += a[i] * b[i];
+    }
+
+    std::lock_guard<std::mutex> locked(result_lock);
+    res += tmp;
+}
+
+
+int main()
+{
+	srand((unsigned) time(0));
+    std::vector<int> v1, v2;
+    for (int i = 0; i < N; i++) {
+        v1.push_back(rand()%100+1);
+        v2.push_back(rand()%100+1);
+    }
+    unsigned int nthreads = std::thread::hardware_concurrency();
+    std::vector <std::thread> threads;
+    int res = 0;
+    int step = v1.size()/nthreads;
+    int first = 0;
+    int last = step + v1.size()%nthreads;
+
+    for (unsigned i = 0; i < nthreads; i++) {
+        threads.push_back(std::thread(calc, v1, v2, first, last, std::ref(res)));
+        first = last;
+        last = first + step;
+    }
+    for( auto& t: threads ) {
+        t.join();
+    }
+
+    std::cout << "Result: " << res << std::endl;
+	system("pause");
+    return 0;
+}
